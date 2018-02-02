2c8f831252cf0624cfe9124f7af7a16003216173
Vincent Humeau <hello@vincent-humeau.com>
2018-02-02 10:47:17 +0000

    Start making function to show colors in kss

---
+++ static/js/kss.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  kssShowColors();
+});
+
+function getCssVar(prefix, value) {
+  var htmlStyles = window.getComputedStyle(document.querySelector("html"));
+  var cssVar = "--" + prefix + "-" + value;
+  var cssVarResult = htmlStyles.getPropertyValue(cssVar);
+
+  return cssVarResult;
+}
+
+function kssShowColors() {
+  var colorVariation = ["base", "dark", "light", "transparent", "contrast"];
+
+  var elements = document.querySelectorAll("#kss-map-colors .kss-color-item");
+  Array.prototype.forEach.call(elements, function(el, i) {
+    var kssColorItemBlock = el;
+    var color = el.getAttribute("data-color");
+    var colorPrefix = "color";
+
+    Array.prototype.forEach.call(colorVariation, function(el, i) {
+      var subValue = el;
+      var value = color + "-" + subValue;
+      var colorVal = getCssVar(colorPrefix, value);
+      var colorItemHtml =
+        '<div class="kss-color-item-child" style="background-color:' + colorVal +'">' +
+        '<p>color(' + color + ', ' + subValue +')</p>' +
+        '<p>' + colorVal + '</p>' +
+        '<p>' + colorVal +'</p>' +
+        '</div>';
+      console.log(colorItemHtml);
+      kssColorItemBlock.insertAdjacentHTML( 'afterend', colorItemHtml );
+    });
+  });
+}
