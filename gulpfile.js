a0bb9ac6365f3090954048a6c5f406bf837d315f
Vincent Humeau <hello@vincent-humeau.com>
2018-08-09 17:01:17 +0100

    Removed gulpfile

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-/// <binding ProjectOpened='jsWatch, scssWatch' />
-const requireDir = require('require-dir');
-
-// Require all tasks in /tasks, including subfolders
-requireDir('./Tasks', { recurse: true });
